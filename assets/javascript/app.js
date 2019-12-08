e7df1e5aa3fa7a057e88c4e2fdfdd72f849d3334
Karlelisa <karlelisaw@gmail.com>
2019-12-08 18:53:39 -0500

    adding disney audio clips

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,71 +1,81 @@
 //Cited:I used the Trivia Game homework solution as a guideline as well as viewing the following:
 //For the timer countdown: https://youtu.be/MLtAMg9_Svw, Quiz App with JavaScript: https://youtu.be/riDzcEQbX6k
-//Cited: https://github.com/soniabradley/Totally-Trivia/blob/master/assets/javascript/app.js
+
 
 $(document).ready(function () {
 
 
-    // Set of Questions, Answers, correct answers and images
-    let questions = [
-        {
+    // Set of Questions, Answers, correct answers, images and audio clips
+    let questions = [{
             question: "Who was the first Disney princess?",
             answers: ["Snow White", "Ariel", "Cinderella", "Belle"],
             correctAnswer: "Snow White",
-            image: "assets/images/snow-white-dancing.gif",
+            image: "assets/images/snow-white-with-bird.gif",
+            audio: "assets/audio/(snow-white-vocalizing-with-the-birds).mp3"
+
         },
         {
             question: "What are the names of Cinderella's evil stepsisters?",
             answers: ["Anastasia & Drizella", "Beatrice & Daphine", "Drizella & Beatrice", "Daphine & Anastasia"],
             correctAnswer: "Anastasia & Drizella",
-            image: "assets/images/cinderella-evil-stepsisters.gif"
+            image: "assets/images/cinderella-evil-stepsisters.gif",
+            audio: "assets/audio/thatsMySlash.mp3"
         },
         {
             question: "What is Simba's mother's name, in The Lion King?",
             answers: ["Narabi", "Akini", "Sarabi", "Zulu"],
             correctAnswer: "Sarabi",
-            image: "assets/images/simba-mom-licking.gif"
+            image: "assets/images/simba-mom-licking.gif",
+            audio: "assets/audio/from-the-day-we-arrive-on-the-planet-and-blinking-step-into-the-sun.mp3"
         },
         {
             question: "In Aladdin, how long was Genie stuck in the lamp?",
             answers: ["20,000 years", "10,000 years", "15,000 years", "25,000 years"],
             correctAnswer: "10,000 years",
-            image: "assets/images/aladdin-genie.gif"
+            image: "assets/images/aladdin-genie.gif",
+            audio: "assets/audio/2you-aint-never-had-a-friend-like-me.mp3"
         },
         {
             question: "What was the last movie that Walt Disney was able to work on before he died?",
             answers: ["101 Dalmations", "Sleeping Beauty", "The Little Mermaid", "The Jungle Book"],
             correctAnswer: "The Jungle Book",
-            image: "assets/images/the-jungle-book.gif"
+            image: "assets/images/the-jungle-book.gif",
+            audio: "assets/audio/2ndthe-bare-necessities.mp3"
         },
         {
             question: "Who was Bambi's best friend?",
             answers: ["Hopper", "Thumper", "Jumper", "Bumper"],
             correctAnswer: "Thumper",
-            image: "assets/images/bambi-and-thumper.gif"
+            image: "assets/images/bambi-and-thumper.gif",
+            audio: "assets/audio/2nd-bambiOnTheIce.mp3"
         },
         {
             question: "Where do the lions live in The Lion King?",
             answers: ["Pride Kingdom", "Glory Rock", "Pride Rock", "The Kingdom"],
             correctAnswer: "Pride Rock",
-            image: "assets/images/simba-mufasa-pride-rock.gif"
+            image: "assets/images/simba-mufasa-pride-rock.gif",
+            audio: "assets/audio/kings-time-as-ruler-rises-and-falls-like-the-sun.mp3"
         },
         {
             question: "What country is Belle from in Beauty and the Beast?",
             answers: ["United Kingdom", "France", "Italy", "Swizterland"],
             correctAnswer: "France",
-            image: "assets/images/belle-walking-on-bridge.gif"
+            image: "assets/images/belle-singing.gif",
+            audio: "assets/audio/no-sir-not-me-i-guarantee-it-i-want-much-more-than-this-provincial-life.mp3"
         },
         {
             question: "In The Litte Mermaid, what does Ariel call the fork in her collection of human objects?",
             answers: ["Wumbus", "Sneedle", "Snarfblatt", "Dinglehopper"],
             correctAnswer: "Dinglehopper",
-            image: "assets/images/ariel-brushing-hair-with-fork.gif"
+            image: "assets/images/ariel-with-fork.gif",
+            audio: "assets/audio/a-dingle-hopper.mp3"
         },
         {
             question: "Who is the only Disney princess with a tattoo?",
             answers: ["Jasmine", "Pocahontas", "Mulan", "Elsa"],
             correctAnswer: "Pocahontas",
-            image: "assets/images/pocahontas-leaves-blowing.gif"
+            image: "assets/images/pocahontas-leaves-blowing.gif",
+            audio: "assets/audio/pocahontas-colors-of-the-wind.mp3"
         }
     ];
 
@@ -102,8 +112,8 @@ $(document).ready(function () {
         game.html("<h2>" + questions[currentQuestion].question + "</h2>");
 
         for (let i = 0; i < questions[currentQuestion].answers.length; i++) {
-            game.append("<button class='answer-button' id='button' data-name='" + questions[currentQuestion].answers[i]
-                + "'>" + questions[currentQuestion].answers[i] + "</button>");
+            game.append("<button class='answer-button' id='button' data-name='" + questions[currentQuestion].answers[i] +
+                "'>" + questions[currentQuestion].answers[i] + "</button>");
         }
     };
 
@@ -124,12 +134,11 @@ $(document).ready(function () {
 
         game.html("<h2>Out of Time!!</h2>");
         game.append("<h3>The Correct Answer was: " + questions[currentQuestion].correctAnswer);
-        game.append("<img src='" + questions[currentQuestion].image + "'/>");
+        game.append("<img src='" + questions[currentQuestion].image + "'/>" + "<audio autoplay src='" + questions[currentQuestion].audio + "'/audio>");
 
         if (currentQuestion === questions.length - 1) {
             setTimeout(endResults, 4 * 2000);
-        }
-        else {
+        } else {
             setTimeout(nextQuestion, 4 * 2000);
         }
     };
@@ -157,8 +166,7 @@ $(document).ready(function () {
         clearInterval(timer);
         if ($(e.target).attr("data-name") === questions[currentQuestion].correctAnswer) {
             answeredCorrectly();
-        }
-        else {
+        } else {
             wrongAnswer();
         }
     };
@@ -176,8 +184,7 @@ $(document).ready(function () {
 
         if (currentQuestion === questions.length - 1) {
             setTimeout(endResults, 4 * 2000);
-        }
-        else {
+        } else {
             setTimeout(nextQuestion, 4 * 2000);
         }
     };
@@ -190,13 +197,12 @@ $(document).ready(function () {
         correct++;
 
         game.html("<h2>Correct!!</h2>");
-        game.append("<img src='" + questions[currentQuestion].image + "' height='270px'/>");
+        game.append("<img src='" + questions[currentQuestion].image + "' height='270px'/>" + "<audio autoplay src='" + questions[currentQuestion].audio + "'/audio>");
 
         if (currentQuestion === questions.length - 1) {
-            setTimeout(endResults, 6000);
-        }
-        else {
-            setTimeout(nextQuestion, 6000);
+            setTimeout(endResults, 7000);
+        } else {
+            setTimeout(nextQuestion, 7000);
         }
     };
 
@@ -209,6 +215,7 @@ $(document).ready(function () {
         incorrect = 0;
         loadQuestion();
     };
+
 
 
 
@@ -230,5 +237,3 @@ $(document).ready(function () {
 
 
 });
-
-
